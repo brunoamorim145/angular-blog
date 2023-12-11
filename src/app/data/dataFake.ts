99e87641e6c446daae1d3e7bda0a59be4f395c55
brunoamorim145 <brunoamorim145@gmail.com>
2023-12-11 16:52:48 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "NOVO HOMEM DE FERRO EM 3D",
+    "description": "Marvel anuncia um novo filme do homem de ferro, confira",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/80C64C0B63382CD3ED2653356125F275F63D036028A77D38DC3286AD851A6833/scale?width=506&aspectRatio=2.00&format=jpeg"
+  },
+  {
+    "id":"2",
+    "title": "Nova Serie Anunciada no Disney +",
+    "description": "abcd abcdef",
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  }
+]
+
